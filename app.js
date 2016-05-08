f4ecd3765c4217b57b1fd27a9705dc9d667a2805
Ruchi1496 <ruchi1496@gmail.com>
2016-05-08 18:57:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,19 +4,10 @@ $('#sidebar-button').click(function(){
 	if ($this.hasClass('show')) {
 		$this.text('open');
 		$(".sidebar").animate({"left" : "-=170px"},"slow");
-		//$this.animate({"left" : "-=170px"},"slow");
+		
 	}
 	else{
 		$this.text('close');
 		$(".sidebar").animate({"left" : "+=170px"},"slow");
 	}
 });
-/*
-(function(){
-		var parallex=$('div.front--bg');
-		$(document).on('scroll',function(){
-			var currScrollPos = $(document).scrollTop();
-			parallex.css('background-position','0' + -currScrollPos/64 + 'px');
-		});
-	})();
-*/
